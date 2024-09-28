15ed05e88c95d60e236c58b1c5003a483ad47c58
Wagnerjr06 <w.pacheco@pucp.edu.pe>
2024-09-28 16:38:21 +0000

    Practica2

--- EjerciciosPC2.py
+++ EjerciciosPC2.py
@@ -79,7 +79,113 @@ for num in range(100):
     if primo(num):
         suma_primos += num
 print ("La suma de todos los números primos menores que 100 es: ", suma_primos)
+
+### Ejercicio 6
+
+def fibonacci_secuencia():
+    fib_secuencia= [0,1]
+    while True:
+        next_fib = fib_secuencia[-1] + fib_secuencia[-2]
+        if next_fib > 50:
+            break
+        fib_secuencia.append(next_fib)
+    return fib_secuencia
+
+fib_numbers = fibonacci_secuencia()
+print(fib_numbers)
+
+### Ejercicio 7
+
+def numero_perfecto(num):
+    suma_divisores=0
+    for i in range(1,num):
+        if num % i == 0:
+            suma_divisores += i
+    return suma_divisores == num
+
+def numeros_perfectos_menores_1000():
+    perfectos = []
+    for num in range(1,1000):
+        if numero_perfecto(num):
+            perfectos.append(num)
+    return perfectos
+
+numeros_perfectos = numeros_perfectos_menores_1000()
+print("Numeros perfectos menores que 1000: ", numeros_perfectos)
+
+### Ejercicio 8
+
+def factorial(n):
+    if n < 0:
+        return "El factorial no está definidi para números negativos"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        resultado = 1
+        for i in range(2,n+1):
+            resultado *= 1
+        return resultado
     
+numero=int(input("Introduce un número entero no negativo: "))
+print(f"El factorial del {numero} es {factorial(numero)}")
+
+### Ejercicio 9
+
+def eliminar_vocales(texto):
+    vocales = "AEIOUaeiou"
+    resultado = ""
+    for caracter in texto:
+        if caracter not in vocales:
+            resultado += caracter
+    return resultado
+
+entrada= input("Introduce un texto: ")
+
+texto_sinvocales = eliminar_vocales(entrada)
+print("Texto sin vocales: ", texto_sinvocales)
+
+### Ejercicio 10
+
+meses = [
+
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+    "Agosto", "Septiembre", "Octubre", "Noviembre" , "Diciembre"
+]
+
+def convertir_fecha(fecha):
+    if "/" in fecha:
+        partes= fecha.split("/")
+        mes = partes [0]
+        dia = partes [1]
+        año = partes [2]
+
+        mes = mes.zfill(2)
+        dia = dia.zfill(2)
+
+        return f"{año}-{mes}-{dia}"
+    else:
+        partes= fecha.replace(",","").split()
+        mes_nombre = partes[0]
+        dia = partes [1]
+        año = partes [2]
+
+        if mes_nombre in meses:
+            mes = str(meses.index(mes_nombre)+ 1).zfill(2)
+        else:
+            return "Mes no válido"
+        
+        dia = dia.zfill(2)
+
+        return f"{año}-{mes}-{dia}"
+    
+entrada_usuario=input("Introduzca una fecha en formato MM/DD/AAAA o 'Mes Día, Año': ")
+
+fecha_convertida = convertir_fecha(entrada_usuario)
+print("Fecha convertida", fecha_convertida)
+
+
+
+
 
 
 
